2e7a1193bf345ef02fd8254d0efe822194b1b3b3
FabricioVeloso <fabraccho@gmail.com>
2026-03-22 17:25:08 -0300

    chore: models init - expor contratos usados pela aplicacao

---
+++ src/personal_setup/models/__init__.py
@@ -0,0 +1,12 @@
+from personal_setup.models.module import CheckResult, ModuleDefinition, ModuleStatus
+from personal_setup.models.plan import PlanResult, PlanStep
+from personal_setup.models.profile import Profile
+
+__all__ = [
+    "CheckResult",
+    "ModuleDefinition",
+    "ModuleStatus",
+    "PlanResult",
+    "PlanStep",
+    "Profile",
+]
